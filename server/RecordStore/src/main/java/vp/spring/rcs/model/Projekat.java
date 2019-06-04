a4075254d21c9e5ad7dfdb66695ae1638c0e90b2
Milan Mirkovic <mirkovicmilan14@yahoo.com>
2019-06-04 20:26:19 +0200

    Napravljeni Modeli

--- server/RecordStore/src/main/java/vp/spring/rcs/model/Projekat.java
+++ server/RecordStore/src/main/java/vp/spring/rcs/model/Projekat.java
@@ -15,11 +15,11 @@ public class Projekat {
 	
 	private String naziv;
 	private String opis;
-	private Katergorija kategorija;
+	private Kategorija kategorija;
 	private Faq faq;
 	private Vest vest;
 	private Korisnik korisnik;
-	public Projekat(Long id, String naziv, String opis, Katergorija kategorija, Faq faq, Vest vest, Korisnik korisnik) {
+	public Projekat(Long id, String naziv, String opis, Kategorija kategorija, Faq faq, Vest vest, Korisnik korisnik) {
 		super();
 		this.id = id;
 		this.naziv = naziv;
@@ -50,10 +50,10 @@ public class Projekat {
 	public void setOpis(String opis) {
 		this.opis = opis;
 	}
-	public Katergorija getKategorija() {
+	public Kategorija getKategorija() {
 		return kategorija;
 	}
-	public void setKategorija(Katergorija kategorija) {
+	public void setKategorija(Kategorija kategorija) {
 		this.kategorija = kategorija;
 	}
 	public Faq getFaq() {
